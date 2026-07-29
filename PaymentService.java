7587db3883714ea1a865869de67b57a9e5279f36
Pavani P Y <pavanipy895@gmail.com>
2026-07-29 15:30:09 +0530

    Fixed payment timeout

---
+++ PaymentService.java
@@ -0,0 +1,9 @@
+public class PaymentService {
+
+    public void processPayment() {
+        System.out.println("Payment processed successfully");
+
+        // Production bug fixed
+        System.out.println("Payment timeout fixed");
+    }
+}
